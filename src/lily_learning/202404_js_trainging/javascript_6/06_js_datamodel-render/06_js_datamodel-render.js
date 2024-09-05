f8edb62d542db73f29d3e2b1744e21a5f4eb3334
朱琦玉 <lilychiou@shuqiyutekiMacBook-Air.local>
2024-09-05 10:41:19 +0800

    JavaScript 系列六：第1課 ── 認識 data model 與 render function

--- src/lily_learning/202404_js_trainging/javascript_6/06_js_datamodel-render/06_js_datamodel-render.js
+++ src/lily_learning/202404_js_trainging/javascript_6/06_js_datamodel-render/06_js_datamodel-render.js
@@ -11,15 +11,15 @@ var todos = [
   ];
   
   function render() {
-    let root = document.querySelector('#root');
+    const root = document.querySelector('#root');
     root.textContent = '';  // 清空之前的內容
   
-    let ul = document.createElement('ul');
+    const ul = document.createElement('ul');
     
     // 使用 forEach 來迭代 todos 陣列中的每個物件
     todos.forEach(function(item) {
-      let li = document.createElement('li');
-      let title = document.createElement('span');
+      const li = document.createElement('li');
+      const title = document.createElement('span');
       title.textContent = item.title;  // 取得 item 中的 title 屬性
       li.append(title);
       ul.append(li);
